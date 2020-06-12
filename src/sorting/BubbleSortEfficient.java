308248ac1007ce88797a140b53ab246c8c3c2b0c
unknown <punithj.mys@gmail.com>
2020-06-12 20:03:49 +0700

    added efficient bubble sort algorithm

--- src/sorting/BubbleSortEfficient.java
+++ src/sorting/BubbleSortEfficient.java
@@ -9,19 +9,19 @@ import java.util.Arrays;
  * @timeComplexity: O(n2)
  * @spaceComplexity: O(1)
  */
-public class BubbleSort {
+public class BubbleSortEfficient {
 
     /** The execution starts from here */
     public static void main(String[] args) {
         System.out.println("<---------------- Demo: Bubble Sort ------------------>");
 
-        /** Generating a random array for testing. Please look into 'SortingUtility.java' for the source code */
+        /** Generating a random array for testing. Please look into 'SortingUtility.java' for the method */
         int[] arr = SortingUtility.generateRandomArray();
 
         System.out.println("Before Sorting: ");
         System.out.println(Arrays.toString(arr));
 
-        bubbleSort(arr);
+        bubbleSortEfficient(arr);
 
         System.out.println("After Sorting: ");
         System.out.println(Arrays.toString(arr));
@@ -31,15 +31,20 @@ public class BubbleSort {
      * @description: The method is used to sort array elements using bubble sort
      * @param arr - an input array to be sorted
      */
-    private static void bubbleSort(int arr[]) {
+    private static void bubbleSortEfficient(int arr[]) {
         for (int i = 0; i < arr.length-1; i++) {
+            boolean swapped = false;
             for (int j = 0; j < arr.length-i-1; j++) {
                 if(arr[j] > arr[j+1]) {
+                    swapped = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
+
+            /** break the loop if the array is already sorted */
+            if(!swapped) break;
         }
     }
 }
